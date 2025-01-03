e62a328331b6e9f8e1d21791ebb22983699d36ec
Pat Welch <pat@mousebrains.com>
2025-01-03 12:19:14 -0800

    Updates from the rail

--- mergeqfiles.py
+++ mergeqfiles.py
@@ -5,7 +5,7 @@
 # The stock script will return a zero length file if the maximum size allowed is smaller
 # than the size of a q-file.
 #
-# This script takes equally spaced in time samples from the 
+# This script takes equally spaced in time samples from the
 # q-files to reach the maximum allowed size.
 #
 # The internal q-file structure is based on Rockland's TN 054
@@ -20,24 +20,29 @@ import numpy as np
 import logging
 import re
 import math
+import sys
 
 def splitHeader(buffer:bytes) -> dict:
     info = {}
     for line in buffer.split(b"\n"):
-        matches = re.fullmatch(b'[#"\s]*([\w_]+)"?\s*=>\s*"?(["\s].*)"?', line.strip())
-        if matches:
-            key = matches[1]
-            val = matches[2]
-            try:
-                key = str(key, "utf-8")
-                val = str(val, "utf-8")
-            except:
-                pass
-            try:
-                val = float(val)
-            except:
-                pass
-            info[key] = val
+        try:
+            line = str(line, "utf-8") # It should be UTF-8
+            matches = re.fullmatch(r'[#"\s]*([\w_]+)"?\s*=>\s*"?(["\s].*)"?', line.strip())
+            if matches:
+                key = matches[1]
+                val = matches[2]
+                try:
+                    key = str(key, "utf-8")
+                    val = str(val, "utf-8")
+                except:
+                    pass
+                try:
+                    val = float(val)
+                except:
+                    pass
+                info[key] = val
+        except:
+            pass # Skip non-UTF8 lines
     return info
 
 def loadQHeader(fn:str) -> tuple:
@@ -84,7 +89,7 @@ def loadQHeader(fn:str) -> tuple:
             return badReturn
         body += buffer
         hdr = splitHeader(buffer)
-        
+
         buffer = fp.read(2)
         if len(buffer) != 2:
             logging.error("Reading data record size, %s != 2, in %s", len(buffer), fn)
@@ -94,7 +99,7 @@ def loadQHeader(fn:str) -> tuple:
 
         hdrSize = fp.tell()
 
-        st = os.fstat(fp.fileno());
+        st = os.fstat(fp.fileno())
         nRecords = math.floor((st.st_size - hdrSize) / dataSize)
         dissLength = hdr["diss_length"] if "diss_length" in hdr else 1
         dt = np.timedelta64(int(hdr["diss_length"] * 1000), "ms")
@@ -122,46 +127,8 @@ def loadQData(f, fn:str, hdr:dict):
 
     stime = (hdr["time"] + np.timedelta64(int(items[2] * 1000), "ms")).astype("datetime64[ns]")
     etime = (hdr["time"] + np.timedelta64(int(items[3] * 1000), "ms")).astype("datetime64[ns]")
-  
+
     return ds
-
-def loadQfile(fn:str):
-    logging.info("Processing %s", fn)
-    records = []
-    with open(fn, "rb") as f:
-        body = bytes()
-        while True:
-            n = f.tell()
-            buffer = f.read(2)
-            if len(buffer) != 2:
-                if buffer is None or len(buffer) == 0: break # EOF
-                logging.error("Unable to read identification field, %s!=2, in %s", len(buffer), fn)
-                break
-            body += buffer
-            (ident,) = struct.unpack("<H", buffer)
-            match ident:
-                case 0x1729: # Header+Config record
-                    logging.info(f"HDR at {n:#05x}")
-                    (hdrSize, dataSize, stime, etime, nRecords, dt) = loadQHeader(ifn)
-                    if hdrSize is None: 
-                        body = None
-                        break
-
-                case 0x1657: # Data record
-                    logging.info(f"Data at {n:#05x}")
-                    rec = loadQData(f, fn, hdr)
-                    if rec is None: break
-                    records.append(rec)
-                case 0x1a1a: # EOF ??
-                    logging.info(f"0x1a1a at {n:#05x}")
-                    break
-                case _:
-                    logging.error(f"Unrecognized record identifier, {ident:#04x}, in %s", fn)
-                    break
-
-    if not records:
-        logging.warning("No records found in %s", fn)
-        return None
 
 def writePartialFile(ifn:str, ofp, szHeader:int, szData:int, nRecords:int, indices:np.array):
     logging.info("Partial file %s sz %s n %s of %s", ifn, szHeader, len(indices), nRecords)
@@ -205,6 +172,10 @@ def decimateFiles(qfiles:dict, ofn:str, totSize:int, maxSize:int):
         logging.info("availSize %s ratio %s", availSize, ratio)
 
         with open(ofn, "wb") as ofp:
+            if ratio <= 0:
+                logging.warning("Creating an empty file since ratio, %s, <=0", ratio)
+                return
+
             for ifn in filenames:
                 item = info[ifn]
                 n = item["nRecords"]
@@ -212,16 +183,16 @@ def decimateFiles(qfiles:dict, ofn:str, totSize:int, maxSize:int):
                         np.floor(
                             np.linspace(0, n, math.floor(n * ratio), endpoint=False)).astype(int)
                         )
-                writePartialFile(ifn, 
-                                 ofp, 
-                                 item["hdrSize"], 
-                                 item["dataSize"], 
+                writePartialFile(ifn,
+                                 ofp,
+                                 item["hdrSize"],
+                                 item["dataSize"],
                                  item["nRecords"],
                                  indices)
     except:
         logging.exception("Unable to decimate %s to %s", filenames, ofn)
 
-def glueFiles(filenames:list[str], ofn:str, bufferSize:int=1024*1024):
+def glueFiles(filenames:list, ofn:str, bufferSize:int=1024*1024):
     try:
         totSize = 0
         with open(ofn, "wb") as ofp:
@@ -237,6 +208,45 @@ def glueFiles(filenames:list[str], ofn:str, bufferSize:int=1024*1024):
     except:
         logging.exception("Unable to glue %s to %s", filenames, ofn)
 
+def scanDirectory(args:ArgumentParser, times:np.array):
+    with os.scandir(args.datadir) as it:
+        qfiles = {}
+        totSize = 0
+        for entry in it:
+            if not entry.name.endswith(".q") or not entry.is_file():
+                continue
+            st = entry.stat()
+            logging.info("%s sz %s ctime %s mtime %s times %s %s",
+                         entry.name,
+                         st.st_size,
+                         np.datetime64(round(st.st_ctime * 1000), "ms"),
+                         np.datetime64(round(st.st_mtime * 1000), "ms"),
+                         times[0].astype("datetime64[s]"),
+                         times[1].astype("datetime64[s]"),
+                         )
+            if st.st_ctime > times[1] or st.st_mtime < times[0]:
+                logging.debug("Ignoring %s due to times out of range, %s>%s %s or %s<%s %s",
+                              entry.name,
+                              np.datetime64(round(st.st_ctime*1e9), "ns"),
+                              np.datetime64(round(times[1] * 1e9), "ns"),
+                              st.st_ctime > times[1],
+                              np.datetime64(round(st.st_mtime*1e9), "ns"),
+                              np.datetime64(round(times[0]*1e9), "ns"),
+                              st.st_mtime < times[0],
+                              )
+                continue
+            qfiles[entry.path] = st.st_size
+            totSize += st.st_size
+
+    logging.info("Total size %s max %s", totSize, args.maxSize)
+    if totSize <= args.maxSize:
+        # Glue the files together since their total size is small enough
+        # This handles the no-files case and will generate an empty .mri file
+        glueFiles(sorted(qfiles), args.mri, args.bufferSize)
+    else:
+        # Parse the qfiles and pull out roughly equally spaced in time records
+        decimateFiles(qfiles, args.mri, totSize, args.maxSize)
+
 parser = ArgumentParser()
 parser.add_argument("stime", type=float, help="Unix seconds for earliest sample, or 0 for now")
 parser.add_argument("dt", type=float, help="Seconds added to stime for other end of samples")
@@ -245,59 +255,44 @@ parser.add_argument("mri", type=str, help="Output filename")
 parser.add_argument("--bufferSize", type=int, default=100*1024,
                     help="Maximum buffer size to read at a time in bytes")
 parser.add_argument("--datadir", type=str, default="~/data", help="Where Q-files are stored")
-parser.add_argument("--verbose", action="store_true", help="Enable logging.debug messages")
-parser.add_argument("--logfile", type=str, default="~/data/mergeqfiles.log",
-                    help="Output of logfile messages")
+parser.add_argument("--verbose", "-v", action="store_true", help="Enable logging.debug messages")
+parser.add_argument("--logfile", "-o", type=str, help="Output of logfile messages")
 # parser.add_argument("--config", type=str, default="~/data/mergeqfiles.config",
                     # help="YAML config file")
 args = parser.parse_args()
 
-if args.logfile: args.logfile = os.path.abspath(os.path.expanduser(args.logfile))
-if args.datadir: args.datadir = os.path.abspath(os.path.expanduser(args.datadir))
+args.datadir = os.path.abspath(os.path.expanduser(args.datadir))
 
-logging.basicConfig(
-        format="%(asctime)s %(levelname)s: %(message)s",
-        level=logging.DEBUG if args.verbose else logging.INFO,
-        filename=args.logfile,
-        )
+if not os.path.isdir(args.datadir):
+    print(f"ERROR: Data directory '{args.datadir}' does not exist")
+    sys.exit(1)
 
-logging.info("Command line, %s", args)
+try:
+    if args.logfile is None:
+        args.logfile = os.path.join(args.datadir, "mergeqfiles.log")
+    elif args.logfile == "":
+        args.logfile = None # Spew out to the console
+    else:
+        args.logfile = os.path.abspath(os.path.expanduser(args.logfile))
+        dirname = os.path.dirname(args.logfile)
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname, 0o755, exist_ok=True)
 
-if args.stime <= 0:
-    args.stime = time.time() # Current time
+    logging.basicConfig(
+            format="%(asctime)s %(levelname)s: %(message)s",
+            level=logging.DEBUG if args.verbose else logging.INFO,
+            filename=args.logfile,
+            )
 
-times = np.sort([args.stime, args.stime + args.dt])
+    logging.info("Args: %s", args)
 
-logging.info("Time limits %s", times.astype("datetime64[s]"))
+    if args.stime <= 0:
+        args.stime = time.time() # Current time
 
-with os.scandir(args.datadir) as it:
-    qfiles = {}
-    totSize = 0
-    for entry in it:
-        if not entry.name.endswith(".q") or not entry.is_file(): continue
-        st = entry.stat()
-        logging.info("%s sz %s ctime %s mtime %s times %s %s", 
-                     entry.name, st.st_size, st.st_ctime, st.st_mtime, 
-                     times[0], times[1])
-        if st.st_ctime > times[1] or st.st_mtime < times[0]: 
-            logging.info("Ignoring %s due to times out of range, %s>%s %s or %s<%s %s",
-                         entry.name,
-                         np.datetime64(round(st.st_ctime*1e9), "ns"),
-                         np.datetime64(round(times[1] * 1e9), "ns"), 
-                         st.st_ctime > times[1],
-                         np.datetime64(round(st.st_mtime*1e9), "ns"),
-                         np.datetime64(round(times[0]*1e9), "ns"),
-                         st.st_mtime < times[0],
-                         )
-            continue
-        qfiles[entry.path] = st.st_size
-        totSize += st.st_size
+    times = np.sort([args.stime, args.stime + args.dt])
 
-logging.info("Total size %s max %s", totSize, args.maxSize)
-if totSize <= args.maxSize:
-    # Glue the files together since their total size is small enough
-    # This handles the no-files case and will generate an empty .mri file
-    glueFiles(sorted(qfiles), args.mri, args.bufferSize) 
-else:
-    # Parse the qfiles and pull out equally spaced in time records
-    decimateFiles(qfiles, args.mri, totSize, args.maxSize)
+    logging.info("Time limits %s", times.astype("datetime64[s]"))
+
+    scanDirectory(args, times)
+except:
+    logging.exception("Unexpected exception executing %s", args)
